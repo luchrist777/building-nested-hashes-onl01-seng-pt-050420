51217ca4ace71c5a42e0f0ce5b996c7726c14a73
Luis Leandro <luchrist777@gmail.com>
2020-05-17 06:37:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,15 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
+ epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      },
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }}
-
-  
+   }}
 end
